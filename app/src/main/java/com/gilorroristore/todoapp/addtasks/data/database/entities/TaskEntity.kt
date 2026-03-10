1ff30dffbf4d1621dd3730cbdd93dc56a378c1bd
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-10 11:42:57 -0600

    mappers for entities

--- app/src/main/java/com/gilorroristore/todoapp/addtasks/data/database/entities/TaskEntity.kt
+++ app/src/main/java/com/gilorroristore/todoapp/addtasks/data/database/entities/TaskEntity.kt
@@ -2,6 +2,7 @@ package com.gilorroristore.todoapp.addtasks.data.database.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.gilorroristore.todoapp.addtasks.ui.model.TaskModel
 
 @Entity(tableName = "task_table")
 data class TaskEntity(
@@ -9,4 +10,10 @@ data class TaskEntity(
     val id: Int,
     val task: String,
     var selected: Boolean = false,
+)
+
+fun TaskEntity.toDomain() = TaskModel(
+    id = id,
+    task = task,
+    selected = selected
 )
--- app/src/main/java/com/gilorroristore/todoapp/addtasks/ui/model/TaskModel.kt
+++ app/src/main/java/com/gilorroristore/todoapp/addtasks/ui/model/TaskModel.kt
@@ -1,7 +1,15 @@
 package com.gilorroristore.todoapp.addtasks.ui.model
 
+import com.gilorroristore.todoapp.addtasks.data.database.entities.TaskEntity
+
 data class TaskModel(
-    val id: Long = System.currentTimeMillis(),
+    val id: Int = System.currentTimeMillis().hashCode(),
     val task: String,
     var selected: Boolean = false,
+)
+
+fun TaskModel.toDatabase() = TaskEntity(
+    id = id,
+    task = task,
+    selected = selected
 )
