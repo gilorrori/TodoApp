793f1be01278f86b3197045045cdce8c0328cb61
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-10 22:35:50 -0600

    new querys for tasks dao

--- app/src/main/java/com/gilorroristore/todoapp/addtasks/data/database/dao/TaskDao.kt
+++ app/src/main/java/com/gilorroristore/todoapp/addtasks/data/database/dao/TaskDao.kt
@@ -1,8 +1,10 @@
 package com.gilorroristore.todoapp.addtasks.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.gilorroristore.todoapp.addtasks.data.database.entities.TaskEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -13,4 +15,10 @@ interface TaskDao {
 
     @Insert
     suspend fun addTask(taskEntity: TaskEntity)
+
+    @Update
+    suspend fun updateTask(taskEntity: TaskEntity)
+
+    @Delete
+    suspend fun deleteTask(taskEntity: TaskEntity)
 }
