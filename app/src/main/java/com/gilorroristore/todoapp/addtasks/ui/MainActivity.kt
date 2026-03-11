06414e4b255b3b8ccf70253ec71c8b3f213384b3
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-11 11:51:06 -0600

    package refactor for MainActivity.kt

--- app/src/main/java/com/gilorroristore/todoapp/addtasks/ui/MainActivity.kt
+++ app/src/main/java/com/gilorroristore/todoapp/addtasks/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.gilorroristore.todoapp
+package com.gilorroristore.todoapp.addtasks.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,11 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.gilorroristore.todoapp.addtasks.ui.TasksScreen
 import com.gilorroristore.todoapp.ui.theme.TodoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -29,4 +25,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
