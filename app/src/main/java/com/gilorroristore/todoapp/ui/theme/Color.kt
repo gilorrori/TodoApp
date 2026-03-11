fad6de3dcd68aaba7c4a0bf0bda3fde9062c1779
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-03-11 11:51:40 -0600

    new colors for theme

--- app/src/main/java/com/gilorroristore/todoapp/ui/theme/Color.kt
+++ app/src/main/java/com/gilorroristore/todoapp/ui/theme/Color.kt
@@ -6,6 +6,9 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val White20 = Color(0xF2FFFFFF)
+val Black20 = Color(0x54000000)
--- app/src/main/java/com/gilorroristore/todoapp/ui/theme/Theme.kt
+++ app/src/main/java/com/gilorroristore/todoapp/ui/theme/Theme.kt
@@ -20,7 +20,9 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -28,7 +30,7 @@ private val LightColorScheme = lightColorScheme(
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
+
     onSurface = Color(0xFF1C1B1F),
     */
 )
